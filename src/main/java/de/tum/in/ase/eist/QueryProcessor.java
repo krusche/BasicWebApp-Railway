5eaed11b92297bfdd327f2e94c1245b4eab49575
Santiago Berrezueta <santiastic@outlook.com>
2023-06-27 22:14:05 +0200

    second update

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Santiago's Team";
+           return "Santiago";
         } else { // TODO extend the programm here
             return "";
         }
